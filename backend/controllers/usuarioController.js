4b9a874e76329816822449732f3837cb9365f118
Adotel <adriandotel@gmail.com>
2022-08-26 11:38:44 +0200

    Creada función de autenticar con sus comprobaciones

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,5 +1,6 @@
 
 import Usuario from "../models/Usuario.js"
+import generarID from "../helpers/generarID.js";
 
 const registrar = async (req, res) => {
 
@@ -18,6 +19,7 @@ const registrar = async (req, res) => {
     try{
         // Crea el nuevo Usuario a través del modelo de moongose
         const usuario = new Usuario(req.body)
+        usuario.token = generarID()
         // Se almacena con .save()
         const usuarioAlmacenado = await usuario.save()
         res.json(usuarioAlmacenado)
@@ -29,6 +31,43 @@ const registrar = async (req, res) => {
     } 
 }
 
+const autenticar = async (req, res) => {
+
+    const { email, password } = req.body
+
+    // Buscar si existe el usuario en Mongo
+    const usuario = await Usuario.findOne({
+        email
+    })
+
+    // Comprobar si el usuario existe
+    if(!usuario) {
+        const error = new Error("El usuario no existe");
+        return res.status(404).json({msg: error.message})
+    }
+
+    // Comprobar si el usario está confirmado
+    if(!usuario.confirmado) {
+        const error = new Error("La Cuenta no ha sido confirmada");
+        return res.status(403).json({msg: error.message})
+    }
+
+    // Comprobar su password
+    if(await usuario.comprobarPassword(password)) {
+        res.json({
+            _id : usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            
+        })
+    } else {
+        const error = new Error("Contraseña incorrecta");
+        return res.status(403).json({msg: error.message})
+    }
+
+}
+
 export {
-    registrar
+    registrar,
+    autenticar
 }

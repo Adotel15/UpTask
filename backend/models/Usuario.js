b3ba53fe6208061bf230540ed4c9d10fee01a7c2
Adotel <adriandotel@gmail.com>
2022-08-26 11:35:50 +0200

    Función para encriptar contraseña, y funcion para comprobar si la contraseña que se introduce coincide con la que está en la base de datos

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,5 +1,6 @@
 
 import mongoose from "mongoose";
+// Libreria para encriptar la contraseña
 import bcrypt from "bcrypt"
 
 // Modelo Usuario
@@ -44,6 +45,27 @@ const usuarioSchema = mongoose.Schema({
     timestamps: true,
    }
 )
+
+// Antes de que se guarde, vamos a encriptar la contraseña
+usuarioSchema.pre('save', async function(next) {
+
+    if (!this.isModified('password')) {
+        // Si la contraseña se ha modificado no vamos a volver a hashearla
+        // Usamos next() para que siga adelante la ejecución sin las lineas de abaajo
+        next();
+    }
+    // Variable que "hashea 10 veces"
+    const salt = await bcrypt.genSalt(10);
+    // Guardamos el password hasheado
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+// Para crear metódos propios del para el Schema
+usuarioSchema.methods.comprobarPassword = async function(passwordFormulario){
+    // Va a comprobar si el hash de la contraseña que está pasando el usuario es igual
+    // que el que está en la base de datos
+    return await bcrypt.compare(passwordFormulario, this.password)
+}
 
 // Creamos el modelo propiamente dicho
 const Usuario = mongoose.model("Usuario", usuarioSchema)

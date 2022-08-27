27b2bd60abea31aa5e7c880ed57243caa80c90e5
Adotel <adriandotel@gmail.com>
2022-08-27 17:27:53 +0200

    Base Front creado, instalado tailwindcss

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/*                      Instalar TailwindCSS                         */
+/*  1.  npm i -D tailwindcss postcss autoprefixer                    */
+/*  2.  npx tailwindcss init -p                                      */
+/*  3.  Agregar { content: ["index.html", "./src/estrellas/*.jsx"],} */
+/*  4.  index.css @tailwind base, components, utilities              */
